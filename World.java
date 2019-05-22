717f0f75059f2f9c349f98c56239759574fc5f71
Myles Lazarus <myles@abnproperty.com>
2019-05-22 21:23:41 +0000

    keys

--- World.java
+++ World.java
@@ -1,2 +1,85 @@
-public class World{
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Canvas;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.awt.event.ActionListener;
+
+
+public class World extends Canvas implements KeyListener, Runnable{
+  
+  private ArrayList<Player> players;
+  private boolean[] keys;
+
+  public World(){
+	keys = new boolean[4];
+  }
+
+
+
+
+
+
+
+
+  public void keyPressed(KeyEvent e){
+
+
+    if (e.getKeyCode() == KeyEvent.VK_LEFT)
+    {
+      keys[0] = true;
+    }
+    if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+    {
+      keys[1] = true;
+    }
+    if (e.getKeyCode() == KeyEvent.VK_UP)
+    {
+      keys[2] = true;
+    }
+    if (e.getKeyCode() == KeyEvent.VK_DOWN)
+    {
+      keys[3] = true;
+    }
+  }
+
+  public void keyReleased(KeyEvent e){
+
+   if (e.getKeyCode() == KeyEvent.VK_LEFT)
+    {
+      keys[0] = false;
+    }
+    if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+    {
+      keys[1] = false;
+    }
+    if (e.getKeyCode() == KeyEvent.VK_UP)
+    {
+      keys[2] = false;
+    }
+    if (e.getKeyCode() == KeyEvent.VK_DOWN)
+    {
+      keys[3] = false;
+    }
+  }
+
+  public void keyTyped(KeyEvent e){}
+
+  public void run()
+  {
+    try
+    {
+      while(true)
+      {
+        Thread.currentThread().sleep(5);
+        repaint();
+      }
+    }catch(Exception e)
+    {
+    }
+  }
+
 }
