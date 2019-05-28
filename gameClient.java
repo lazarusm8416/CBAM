e7026486877caa6f9ebaf55be29194b60759b20e
Brian Zhu <abzhu314@gmail.com>
2019-05-28 13:39:16 -0700

    Update gameClient.java

--- gameClient.java
+++ gameClient.java
@@ -14,14 +14,10 @@ public class gameClient {
         address = InetAddress.getByName("localhost");
     }
  
-    public String send(String msg) {
+    public void send(String msg) {
         buf = msg.getBytes();
         DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 4445);
         socket.send(packet);
-        packet = new DatagramPacket(buf, buf.length);
-        socket.receive(packet);
-        String received = new String(packet.getData(), 0, packet.getLength());
-        return received;
     }
  
     public void close() {
