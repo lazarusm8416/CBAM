66cb26fba0875a23c51d2b8e264bcf7ffe08d4e4
Brian Zhu <abzhu314@gmail.com>
2019-05-29 14:47:22 -0700

    Update UserClient.java

--- UserClient.java
+++ UserClient.java
@@ -1,19 +1,90 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.URL;
 import java.util.Scanner;
 
 public class UserClient implements Runnable {
 
-	private String host;
 	private static Socket socket;
 	private int id;
-	
-	public UserClient(String host) {
-		this.host = host;
+	private PrintWriter output;
+	private String name;
+	private Graphics window;
+
+	public Graphics getWindow() {
+		return window;
 	}
-	
-	public static void main(String[] args) throws IOException {
+
+	public void draw(Graphics window, String direction, int x, int y, String name) {
+		while (!(window instanceof Graphics)) {
+			return;
+		}
+		URL url = getClass().getResource("playerRight.png");
+		if (direction.equals("RIGHT")) {
+			url = getClass().getResource("playerLeft.png");
+		}
+		Image image = null;
+		try {
+			image = ImageIO.read(url);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		if (!this.name.equals(name)) {
+			window.drawImage(image, x, y, 50, 50, null);
+		}
+	}
+
+	public UserClient() {
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Enter a Hostname Address: ");
+		String hostname = keyboard.nextLine();
+		System.out.println("Enter a Username: ");
+		String name = keyboard.nextLine();
+		this.name = name;
+		if (!(hostname instanceof String)) {
+			System.out.println("Invalid Hostname Arguments");
+			return;
+		}
+		try {
+			socket = new Socket(hostname, Server.port);
+			//Scanner input = new Scanner(socket.getInputStream());
+			//System.out.println("Server: " + input.nextLine());
+			output = new PrintWriter(socket.getOutputStream(), true);
+			broadcastMessage(name, " joined the game!");
+			//Scanner sockscan = new Scanner(socket.getInputStream());
+			//Thread t = new Thread(new UserClient(hostname));
+			Thread t = new Thread(this);
+			t.start();
+			//while (true) {
+				//BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				if (keyboard.hasNextLine()) {
+					broadcastMessage(name, keyboard.nextLine());
+				}
+				// fix printing multiples, and print overlapping from multiples issue
+				/*while (reader.ready()) {
+					System.out.println(sockscan.nextLine());
+				}*/
+			//}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void broadcastMessage(String name, String s) {
+		output.println(name + ": " + s);
+	}
+	public void broadcastMessage(String s) {
+		output.println(s);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	/*public static void main(String[] args) throws IOException {
 		Scanner keyboard = new Scanner(System.in);
 		System.out.println("Enter a Hostname Address: ");
 		String hostname = keyboard.nextLine();
@@ -36,10 +107,14 @@ public class UserClient implements Runnable {
 				output.println(name + ": " + keyboard.nextLine());
 			}
 			// fix printing multiples, and print overlapping from multiples issue
-			/*while (reader.ready()) {
-				System.out.println(sockscan.nextLine());
-			}*/
+			//while (reader.ready()) {
+			//	System.out.println(sockscan.nextLine());
+			//}
 		}
+	}*/
+
+	public void setWindow(Graphics window) {
+		this.window = window;
 	}
 	
 	@Override
@@ -56,6 +131,16 @@ public class UserClient implements Runnable {
 					String s = sockscan.nextLine();
 					if (s.contains("Your Client ID")) {
 						id = Integer.valueOf(s.replace("Your Client ID is: ",""));
+					} else if (s.contains("moved to") && !s.contains(":")) {
+						String position = s.replace("moved to ", "");
+						position = position.replace("[]", ",");
+
+						position = position.replace("][", ",");
+						String name = String.valueOf(position.split(",")[0]);
+						int x = Integer.valueOf(position.split(",")[1]);
+						int y = Integer.valueOf(position.split(",")[2]);
+						String direction = String.valueOf(position.split(",")[3]);
+						this.draw(window, direction, x, y, name);
 					} else {
 						System.out.println(s);
 					}
@@ -67,6 +152,8 @@ public class UserClient implements Runnable {
 			e.printStackTrace();
 		}
 	}
+
+
 
 	@Override
 	protected void finalize() {
