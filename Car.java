2f95a2dd63eae971eb3a7f324cd10124afffd8ae
parkc5094 <47994407+parkc5094@users.noreply.github.com>
2019-05-30 14:14:06 -0700

    car pic

--- Car.java
+++ Car.java
@@ -1,5 +1,12 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.io.File;
+import java.net.URL;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.imageio.ImageIO;
+
 
 public class Car extends ColidableObject
 {
