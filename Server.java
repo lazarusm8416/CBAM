9daef6ff6ebd9d752478d468c9cd9043c729383b
Brian Zhu <abzhu314@gmail.com>
2019-06-03 14:44:10 -0700

    Update Server.java

--- Server.java
+++ Server.java
@@ -51,7 +51,10 @@ public class Server {
 			ClientThread w;
 			try {
 				Socket socket = server.accept();
-				w = new ClientThread(new Client(socket, currentId), textArea, this);
+				BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				String name = reader.readLine();
+				System.out.println(name);
+				w = new ClientThread(new Client(socket, currentId), textArea, this, name);
 				clients.add(w);
 				Thread thread = new Thread(w);
 				thread.start();
