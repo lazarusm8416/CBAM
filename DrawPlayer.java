e6d5325f9b883a682100c941848f1df776bc406b
Brian Zhu <abzhu314@gmail.com>
2019-06-04 13:52:54 -0700

    Update DrawPlayer.java

--- DrawPlayer.java
+++ DrawPlayer.java
@@ -2,16 +2,12 @@ import java.awt.*;
 
 public class DrawPlayer extends ColidableObject{
 
-    private int x;
-    private int y;
     private Player player;
     private String name;
     private Image image;
 
     public DrawPlayer(int x, int y, String name) {
         super(x,y,50,30);
-        this.x = x;
-        this.y = y;
         this.name = name;
     }
 
@@ -19,24 +15,8 @@ public class DrawPlayer extends ColidableObject{
         return image;
     }
 
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public void setY(int y) {
-        this.y = y;
     }
 
     public void setImage(Image i) {
