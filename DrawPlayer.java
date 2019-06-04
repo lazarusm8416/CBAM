cc9a0de3c6ea65263c88cf05a6e9711075ffa17e
Brian Zhu <abzhu314@gmail.com>
2019-06-04 14:05:18 -0700

    Update DrawPlayer.java

--- DrawPlayer.java
+++ DrawPlayer.java
@@ -1,14 +1,12 @@
 import java.awt.*;
 
-public class DrawPlayer extends ColidableObject{
+public class DrawPlayer extends Player{
 
-    private Player player;
     private String name;
     private Image image;
-    private int lilS;
 
     public DrawPlayer(int x, int y, String name) {
-        super(x,y,50,30);
+        super(x,y,50,30,3,null);
         this.name = name;
     }
 
@@ -23,51 +21,5 @@ public class DrawPlayer extends ColidableObject{
     public void setImage(Image i) {
         image = i;
     }
-    public void bounce(String dir){
-	    decel();
-	    for (int i =0; i<10; i++){
-		    move(dir);
-	    }
-	    decel();
-    }
-    public void bump(Object p){
-	    if (getY()==p.getY()+p.getH()){ //this top bounce
-		    if ( (getX() > p.getX() && getX() < p.getX()+p.getW()) || (getX()+getW() > p.getX() && getX()+getW() < p.getX()+p.getW()) ){
-			    bounce("down");
-			    p.bounce("up");
-		    }
-	    }
-
-	    if (getX()==p.getX()+p.getW()){ // this left bounce
-		    if ( (getY() > p.getY() && getY() < p.getY()+p.getH()) || (getY()+getH() > p.getY() && getY()+getH() < p.getY()+p.getH()) ){
-			    bounce("left");
-			    p.bounce("right");
-		    }
-	    }
-    }
-    public void move (String dir){
-	    if (dir.equals("up"))
-		    setY(getY()-speed);
-	    if (dir.equals("down"))
-		    setY(getY()+speed);
-	    if (dir.equals("left"))
-		    setX(getX()-speed);
-	    if (dir.equals("right"))
-		    setX(getX()+speed);
-	    if (speed<=20)
-		    lilS();
-    }
-    public void lilS(){
-	    lilS+=1;
-	    if (lilS==20){
-		    speed+=1;
-		    lilS=0;
-	    }
-    }
-	
-    public void decel(){
-	    speed=1;
-	    lilS=0;
-    }
-
+    
 }
