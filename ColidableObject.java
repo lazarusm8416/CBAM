6665382a8115b53aa81ec560df7e4b8bee060072
samaryam <aflather@roadrunner.com>
2019-06-10 14:42:42 -0700

    collide

--- ColidableObject.java
+++ ColidableObject.java
@@ -68,9 +68,9 @@ public abstract class ColidableObject
   
   public boolean didCollideLeft(ColidableObject o)
   {
-	  if(  (yPos>o.getY()&&yPos<o.getY()+o.getH())) //|| (yPos+height>o.getY()&&yPos+height<o.getY()+o.getH()) )//heac on collision in y axis.
+	  if(  (yPos>o.getY()&&yPos<o.getY()+o.getH()) || (yPos+height>o.getY()&&yPos+height<o.getY()+o.getH()) )//heac on collision in y axis.
 	  {
-		if ( xPos+width<=o.getX() ) // left side touchie.
+		if ( xPos>=o.getX()+o.getW() ) // left side touchie.
 		{
 		  System.out.println("collide left " +o.toString());
 		  return true;
@@ -82,9 +82,9 @@ public abstract class ColidableObject
   
   public boolean didCollideRight(ColidableObject o)
   {
-	  if(  (yPos>o.getY()&&yPos<o.getY()+o.getH())) //|| (yPos+height>o.getY()&&yPos+height<o.getY()+o.getH())  )
+	  if(  (yPos>o.getY()&&yPos<o.getY()+o.getH()) || (yPos+height>o.getY()&&yPos+height<o.getY()+o.getH())  )
 	  {
-		if ( xPos>=o.getX()+o.getW() )
+		if ( xPos+width<=o.getX() )
 		{
 		  System.out.println("collide right " + o.toString());
 		  return true;
@@ -96,7 +96,7 @@ public abstract class ColidableObject
   
   public boolean didCollideTop(ColidableObject o)
   {
-	  if(  (xPos>o.getX()&&xPos<o.getX()+o.getW())) //|| (xPos+width>o.getX()&&xPos+width<o.getX()+o.getW())  )
+	  if(  (xPos>o.getX()&&xPos<o.getX()+o.getW()) || (xPos+width>o.getX()&&xPos+width<o.getX()+o.getW())  )
 	  {
 		if (  yPos<=o.getY()+o.getH() )
 		{
@@ -110,7 +110,7 @@ public abstract class ColidableObject
   
   public boolean didCollideBot(ColidableObject o)
   {
-	  if(  (xPos>o.getX()&&xPos<o.getX()+o.getW())) //|| (xPos+width>o.getX()&&xPos+width<o.getX()+o.getW())  )
+	  if(  (xPos>o.getX()&&xPos<o.getX()+o.getW()) || (xPos+width>o.getX()&&xPos+width<o.getX()+o.getW())  )
 	  {
 		if (yPos+height>=o.getY())
 		{
